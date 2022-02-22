f2a181da0e1dc55a06618ec27e89e608c40ff8ac
amparolopez <amparolopez23@hotmail.com>
2022-02-22 09:35:07 -0300

    1st push

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Hero from './componentes/Hero.js'
+
+function App() {
+  return (
+    <div>
+      <Hero/>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/componentes/Hero.js
@@ -0,0 +1,17 @@
+import React from "react"
+import HeroImg from "../assets/hero.jpg"
+
+
+
+
+const Hero = () => {
+  return (   
+    <div className="containerHero">
+      <img className="Logo" src="HeroImg" />
+        <div class="box-1">
+            <span>FIND YOUR DREAM TRAVEL DESTINATION!</span>
+        </div>
+      </div>
+)
+}
+export default Hero
