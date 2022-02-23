554d1eef453945d25a9554a2be4a499210f05dd8
amparolopez <amparolopez23@hotmail.com>
2022-02-22 23:05:34 -0300

    hero,info

--- frontend/src/componentes/Hero.js
+++ frontend/src/componentes/Hero.js
@@ -1,42 +1,58 @@
 import React from "react"
-import '../App.css';
-import {Navbar, Nav, Container} from 'react-bootstrap';
+import "../App.css"
+import {Navbar, Nav, Container} from "react-bootstrap"
 import logo from "../assets/logo-soho.png"
 
-
 const Hero = () => {
-  return (   
-<>
-<div className="container-fluid">
-  <header>
-  <Navbar bg="transparent" color="white">
-    <Container>
-    <img className="logo" src={require('../assets/logo-soho.png')} />
-    <Nav className="me-auto">
-      <Nav.Link color="white" href="#home"><span>inicio</span></Nav.Link>
-      <Nav.Link href="#nosotos"><span>nosotros</span></Nav.Link>
-      <Nav.Link href="#servicios"><span>servicios</span></Nav.Link>
-      <Nav.Link href="#casosdexito"><span>casos de éxito</span></Nav.Link>
-      <Nav.Link href="#unete"><span>únete al equipo</span></Nav.Link>
-      <Nav.Link href="#contacto"><span>contacto</span></Nav.Link>
-    </Nav>
-    </Container>
-  </Navbar>
-    <div className="containerHero">
-        <div class="maintext">
-            <h1 className="title">Nos especializamos en</h1>
-            <h2 className="title2">interfaces digitales que los usuarios aman</h2>
-            <button className="calltoa">HABLEMOS DE TU PROYECTO</button>
-        </div>
-      </div>
-  </header>
-</div>
-      <div class="event">
-        <h4>EN DIRECTO · <span>SOHO está presente en el </span><span>DIGITALBANK MONTEVIDEO.</span> <span>"Experiencia de Usuario" por Alvaro Añón (SEO de Soho).</span></h4>
+  return (
+    <>
+      <Container fluid className="maintext">
+        <header>
+          <Navbar color="white" expand="md" fixed="top">
+            <Container>
+              <img className="logo" src={require("../assets/logo-soho.png")} />
+              <Navbar.Toggle aria-controls="basic-navbar-nav" />
+              <Navbar.Collapse id="basic-navbar-nav">
+                <Nav>
+                  <Nav.Link color="white" href="#home" className="active">
+                    <span>Inicio</span>
+                  </Nav.Link>
+                  <Nav.Link href="#nosotos">
+                    <span>Nosotros</span>
+                  </Nav.Link>
+                  <Nav.Link href="#servicios">
+                    <span>Servicios</span>
+                  </Nav.Link>
+                  <Nav.Link href="#casosdexito">
+                    <span>Casos de éxito</span>
+                  </Nav.Link>
+                  <Nav.Link href="#unete">
+                    <span>únete al equipo</span>
+                  </Nav.Link>
+                  <Nav.Link href="#contacto">
+                    <span>Contacto</span>
+                  </Nav.Link>
+                </Nav>
+              </Navbar.Collapse>
+            </Container>
+          </Navbar>
+        </header>
+        <Container className="Hero">
+          <h1 className="title">Nos especializamos en</h1>
+          <h2 className="title2">interfaces digitales que los usuarios aman</h2>
+          <button className="calltoa">HABLEMOS DE TU PROYECTO</button>
+        </Container>
+      </Container>
+      <div class="event" dark>
+        <p className="event-text">
+          EN DIRECTO · <span>SOHO está presente en el </span>
+          <span>DIGITALBANK MONTEVIDEO.</span>{" "}
+          <span>"Experiencia de Usuario" por Alvaro Añón (SEO de Soho).</span>
+        </p>
         <button className="btn1">VER EVENTO</button>
         <button className="btn2">PRÓXIMOS EVENTOS</button>
       </div>
-</>
-)
+    </>
+  )
 }
 export default Hero
--- frontend/src/componentes/Info.js
+++ frontend/src/componentes/Info.js
@@ -1,34 +1,62 @@
 import React from "react"
-
+import {Col, Container} from "react-bootstrap"
 
 const Info = () => {
-  return (   
+  return (
     <>
-    <div className="containerInfo">
-      <div className="mainInfo">
-        <h1>17 años de experiencia en pos de tu proyecto</h1>
-        <p> Especializados desde 1996 en usabilidad, experiencia del usuario (UX) y diseño de experiencias de proyectos digitales. Aportamos estrategia e innovación centrada en el usuario y los objetivos de negocio de tu proyecto. Cotrabajando mejoraremos tu tasa de conversión, KPI's, ROI y los resultados de tu actual publicidad PPC (pay per click)</p>
-      </div>
-      <div className="cardsInfo">
-        <div className="card1">
-          <h2>Estrategia, usabilidad & ux </h2>
-          <p>Te ofrecemos excelente usabilidad y experiencia del usuario en tu proyecto, junto a una visión innovadora</p>
+      <div className="containerInfo">
+        <Container className="mainInfo ">
+          <h1 className="mainInfo__title">
+            17 años de experiencia en pos de tu proyecto
+          </h1>
+          <p>
+            Especializados desde 1996 en usabilidad, experiencia del usuario
+            (UX) y diseño de experiencias de proyectos digitales. Aportamos
+            estrategia e innovación centrada en el usuario y los objetivos de
+            negocio de tu proyecto. Cotrabajando mejoraremos tu tasa de
+            conversión, KPI's, ROI y los resultados de tu actual publicidad PPC
+            (pay per click)
+          </p>
+        </Container>
+        <div className="cardsInfo">
+          <Col
+            className="cardInfo"
+            style={{
+              backgroundColor: "#40444c",
+            }}
+          >
+            <h2>Estrategia, usabilidad & ux </h2>
+            <p>
+              Te ofrecemos excelente usabilidad y experiencia del usuario en tu
+              proyecto, junto a una visión innovadora
+            </p>
+          </Col>
+          <Col className="cardInfo" style={{backgroundColor: "#181c24"}}>
+            <h2>Mejoramos la experiencia</h2>
+            <p>
+              Tangibilizamos tu proyecto digital a través de un diseño centrado
+              en el usuario y técnicas avanzadas
+            </p>
+          </Col>
+          <Col className="cardInfo" style={{backgroundColor: "#202a36"}}>
+            <h2>Medición continua de objetivos</h2>
+            <p>
+              Implementación, testing con usuarios y medición continua son
+              necesarias para garantizar el éxito de tu proyecto
+            </p>
+          </Col>
         </div>
-        <div className="card2">
-          <h2>Mejoramos la experiencia</h2>
-          <p>Tangibilizamos tu proyecto digital a través de un diseño centrado en el usuario y técnicas avanzadas</p>
-        </div>
-        <div className="card3">
-          <h2>Medición continua de objetivos</h2>
-          <p>Implementación, testing con usuarios y medición continua son necesarias para garantizar el éxito de tu proyecto</p>
+        <div className="proyectos">
+          <h1>Proyectos destacados</h1>
+          <p>
+            {" "}
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed a felis
+            non sem elementum tempor in at urna. Suspendisse auctor libero ut
+            nibh consequat sed sagittis dolor iaculis.
+          </p>
         </div>
       </div>
-      <div className="proyectos">
-        <h1>Proyectos destacados</h1>
-        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed a felis non sem elementum tempor in at urna. Suspendisse auctor libero ut nibh consequat sed sagittis dolor iaculis.</p>
-      </div>
-    </div>
     </>
-)
+  )
 }
 export default Info
